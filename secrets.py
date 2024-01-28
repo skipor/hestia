640e33bef990e0759e8d0e237aba53bda01bf5d4
Philip Groet <philip.groet@gmail.com>
2024-01-28 16:48:17 +0100

    secrets.py template

---
+++ secrets.py
@@ -0,0 +1,12 @@
+
+
+OWN_CHAT_ID = "AAAAA" # Obtained by messaging the bot and then checking https://api.telegram.org/bot<BOTID>/getUpdates
+TOKEN = "AAAA" # telegram
+
+DB = {
+  "database": "hestia",
+  "host": "hestia-database-dev",
+  "user": "postgres",
+  "password": "mysecretpassword",
+  "port": 5432,
+}
