ed8d56a0547c9814d8a6f143d8844163755aec51
Vladimir Skipor <skipor@nebius.com>
2024-06-16 13:34:20 +0200

    turn on vesteda scaper

--- targets.py
+++ targets.py
@@ -1645,9 +1645,9 @@ class Vesteda(Target):
     url = "https://www.vesteda.com/api/units/search/facet"
     headers = {
       "Content-Type": "application/json",
-      "Referer": "https://www.vesteda.com/en/unit-search?placeType=1&sortType=0&radius=3&s=Amstelveen,%20Nederland&sc=woning&latitude=52.32954207727795&longitude=4.871770866748051&filters=0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847&priceFrom=500&priceTo=9999",
+      "Referer": "https://www.vesteda.com/en/unit-search?placeType=1&sortType=0&radius=3&s=Amstelveen,%20Nederland&sc=woning&latitude=52.348495150540856&longitude=4.8904783499999915&filters=0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847&priceFrom=500&priceTo=9999",
     }
-    body = '{"filters":[0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847],"latitude":52.32954207727795,"longitude":4.871770866748051,"place":"Amstelveen, Nederland","placeObject":{"placeType":"1","name":"Amstelveen, Nederland","latitude":"52.32954207727795","longitude":"4.871770866748051"},"placeType":1,"radius":5,"sorting":0,"priceFrom":500,"priceTo":9999,"language":"en"}'
+    body = '{"filters":[0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847],"latitude":52.348495150540856,"longitude":4.8904783499999915,"place":"Amstelveen, Nederland","placeObject":{"placeType":"1","name":"Amstelveen, Nederland","latitude":"52.348495150540856","longitude":"4.8904783499999915"},"placeType":1,"radius":3,"sorting":0,"priceFrom":500,"priceTo":9999,"language":"en"}'
     r = self.post(url, body, headers)
     results = json.loads(r.content)["results"]["objects"]
 
@@ -2198,6 +2198,7 @@ class WonenBijBouwInvest(Target):
     return homes
 
 targets = [
+  Vesteda(),
   Funda(),
   Spotmakelaardij(),
   Vbtverhuurmakelaars(),
--- tmp/vesteda.sh
+++ tmp/vesteda.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 curl 'https://www.vesteda.com/api/units/search/facet' \
   -H 'content-type: application/json' \
-  -H 'referer: https://www.vesteda.com/en/unit-search?placeType=1&sortType=0&radius=3&s=Amstelveen,%20Nederland&sc=woning&latitude=52.32954207727795&longitude=4.871770866748051&filters=0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847&priceFrom=500&priceTo=9999' \
-  --data-raw '{"filters":[0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847],"latitude":52.32954207727795,"longitude":4.871770866748051,"place":"Amstelveen, Nederland","placeObject":{"placeType":"1","name":"Amstelveen, Nederland","latitude":"52.32954207727795","longitude":"4.871770866748051"},"placeType":1,"radius":5,"sorting":0,"priceFrom":500,"priceTo":9999,"language":"en"}'
+  -H 'referer: https://www.vesteda.com/en/unit-search?placeType=1&sortType=0&radius=3&s=Amstelveen,%20Nederland&sc=woning&latitude=52.348495150540856&longitude=4.8904783499999915&filters=0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847&priceFrom=500&priceTo=9999' \
+  --data-raw '{"filters":[0,6873,6883,6889,6899,6870,6972,6875,6898,6882,6872,6847],"latitude":52.348495150540856,"longitude":4.8904783499999915,"place":"Amstelveen, Nederland","placeObject":{"placeType":"1","name":"Amstelveen, Nederland","latitude":"52.348495150540856","longitude":"4.8904783499999915"},"placeType":1,"radius":3,"sorting":0,"priceFrom":500,"priceTo":9999,"language":"en"}'
